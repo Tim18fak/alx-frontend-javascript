6f338879e083dad7528cbe744707670b61f9592f
Timothy <timothy.avell.olatunde@gmail.com>
2023-09-27 22:53:02 +0000

    promise

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+function handleResponseFromAPI(promise){
+	const body = { status: 200, body: 'success' };
+
+  return promise
+    .then(() => body)
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
+
+}
+
+export default handleResponseFromAPI;
