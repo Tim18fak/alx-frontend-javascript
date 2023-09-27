447c3e6692b64e2d2752d2433e303edac2a62499
Timothy <timothy.avell.olatunde@gmail.com>
2023-09-27 22:32:42 +0000

    js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,22 @@
-function getResponseFromAPI() {
+/*function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     fetch(myNewApi)
       .then((response) => response.json())
       .then((data) => resolve(data))
       .catch((error) => reject(error));
+  });
+}
+
+export default getResponseFromAPI;*/
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // API call logic here
+    // ...
+    if (responseFromAPI) {
+      resolve(responseFromAPI); // resolve with API response
+    } else {
+      reject("Error: API call failed"); // reject with error message
+    }
   });
 }
 
