4a9e0cfe9b82f8b02ff8e65bc010fe1f11ba45f6
Timothy <timothy.avell.olatunde@gmail.com>
2023-09-27 21:42:11 +0000

    js promise

--- 0x01-ES6_promise/1-main.js
+++ 0x01-ES6_promise/1-main.js
@@ -1,6 +1,21 @@
 import getFullResponseFromAPI from './1-promise';
 
-console.log(getFullResponseFromAPI(true));
-console.log(getFullResponseFromAPI(false));
-getFullResponseFromAPI()
+getFullResponseFromAPI(true)
+  .then(response => {
+    // Handle success
+    console.log('Resolved:', response);
+  })
+  .catch(error => {
+    // Handle the error here
+    console.error('Rejected:', error);
+  });
 
+getFullResponseFromAPI(false)
+  .then(response => {
+    // This block will not be executed
+    console.log('Resolved:', response);
+  })
+  .catch(error => {
+    // Handle the error here
+    console.error('Rejected:', error);
+  });
