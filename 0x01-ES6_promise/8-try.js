42c8758b35641d0f2f3b8e9996b079a05516f0a1
Timothy <timothy.avell.olatunde@gmail.com>
2023-09-28 01:28:45 +0000

    js

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,12 @@
+function divideFunction(numerator, denominator) {
+  try {
+    if (denominator === 0) {
+      throw new Error();
+    }
+    return numerator / denominator;
+  } catch (error) {
+    throw Error('cannot divide by 0');
+  }
+}
+
+export default divideFunction;
