46d7dae601302c56db8addb549e6dec7164e8703
Timothy <timothy.avell.olatunde@gmail.com>
2023-09-28 14:59:05 +0000

    jS

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,41 +1,49 @@
-import Currency from './3-currency'
+import Currency from './3-currency';
 
-class Pricing{
-	constructor(amount, currency){
-		this._amount = amount;
-		this._currency = currency;
-	}
-	set currency(currency){
-		this._currency = currency;
-	}
-	set amount(amount){
-		this._amount = amount;
-	}
-	get currency(){
-		return this._currency;
-	}
-	get amount(){
-		return this._amount;
-	}
-	displayFullPrice(){
-		/*return `${this._amount} ${ new Currency${this._currency.name} (${this._currency.name})`*/
-		return `${this._amount} ${new Currency(this._currency.code, this._currency.name).displayFullCurrency()}`;
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-	}
-	static convertPrice(amount,conversionRate){
-		/*try{
-			if(typeof amount !== 'number' && typeof conversionRate !== 'number'){
-				throw new Error()
-			}
-			let newAmount;
-			newAmount = amount * conversionRate;
-			return newAmount;
-		}
-		catch(error){
-			console.log(error)
-		}*/
-		return amount * conversionRate;
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  displayFullPrice() {
+    /*
+  return `${this._amount} ${ new Currency${this._currency.name} (${this._currency.name})`
+*/
+    return `${this._amount} ${new Currency(this._currency.code, this._currency.name).displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    /*
+  * try{
+ * if(typeof amount !== 'number' && typeof conversionRate !== 'number'){
+throw new Error()
 }
+let newAmount;
+newAmount = amount * conversionRate;
+return newAmount;
+}
+catch(error){
+console.log(error)
+} */
+    return amount * conversionRate;
+  }
 }
 
 export default Pricing;
