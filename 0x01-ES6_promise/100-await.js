7a5bd24164d74e08840e3070aef5d2fc47b4f59f
Timothy <timothy.avell.olatunde@gmail.com>
2023-09-28 03:06:32 +0000

    Async/await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,5 +1,10 @@
 import { uploadPhoto, createUser } from './utils'
 
+process.on('unhandledRejection', (err) => {
+  // Handle unhandled promise rejections here
+  console.error(err);
+});
+
 async function asyncUploaderUser(){
 	let photo = await uploadPhoto()
 	let user = await createUser()
