5f51fc6e36beac79514f9f8c027b8b57e278a576
Timothy <timothy.avell.olatunde@gmail.com>
2023-09-28 12:51:00 +0000

    Js

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency'
+
+class Pricing{
+	constructor(amount, currency){
+		this._amount = amount;
+		this._currency = currency
+	}
+	set currency(currency){
+		this._currency = currency
+	}
+	set amount(amount){
+		this._amount = amount
+	}
+	get currency(){
+		return this._currency
+	}
+	get amount(){
+		return this._amount
+	}
+	displayFullPrice(){
+		return `${this._amount} ${this._currency.name} (${this._currency.name})`
+	}
+	static convertPrice(amount,conversionRate){
+		try{
+			if(typeof amount !== 'number' && typeof conversionRate !== 'number'){
+				throw new Error()
+			}
+			let newAmount;
+			newAmount = amount * conversionRate
+		}
+		catch(error){
+			console.log(error)
+		}
+
+}
+}
+
+export default Pricing;
