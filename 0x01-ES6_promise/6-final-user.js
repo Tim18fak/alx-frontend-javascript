cdb493c3d7c76407a1e8a96f2748e802bc433ca4
Timothy <timothy.avell.olatunde@gmail.com>
2023-09-28 01:13:08 +0000

    JS

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,24 +1,33 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 process.on('unhandledRejection', (err) => {
   // Handle unhandled promise rejections here
   console.error(err);
 });
+/*
+function handleProfileSignup(firstName, lastName, fileName) {
+  const userProm = signUpUser(firstName, lastName);
+  const photoProm = uploadPhoto(fileName);
 
-function handleProfileSignup(firstName,lastName,fileName){
-	let userProm = signUpUser(firstName,lastName)
-	let photoProm = uploadPhoto(fileName)
-
-	return Promise.allSettled([userProm,photoProm])
-	.then(results => {
-		return results.map(result => {
-			return { 
-				status: result.status,
-				value: result.status === 'fulfilled' ? result.value : result.reason
-			};
-		});
-	})
+  return Promise.allSettled([userProm, photoProm])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
 
-export default handleProfileSignup;
+export default handleProfileSignup; */
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  const result = [];
+  return Promise.allSettled(promises).then((results) => {
+    results.map(({ status, value, reason }) => (
+      result.push({
+        status,
+        value: status === 'rejected' ? reason.toString() : value,
+      })
+    ));
+    return result;
+  });
+}
