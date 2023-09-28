ee8176fe794fa96935e0a3611ff5ad3fcbc5572a
Timothy <timothy.avell.olatunde@gmail.com>
2023-09-28 00:35:15 +0000

    js

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,10 @@
-function uploadPhoto(fileName){
-	return Promise.reject( new Error(`${fileName} cannot be processed`))
+process.on('unhandledRejection', (err) => {
+  // Handle unhandled promise rejections here
+  console.error(err);
+});
+
+function uploadPhoto(fileName) {
+  return Promise.reject(new Error(`${fileName} cannot be processed`));
 }
 
 export default uploadPhoto;
