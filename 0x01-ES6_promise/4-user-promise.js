ef97cae50bc580d4d66b1cb6a6c647d793ffb9d6
Timothy <timothy.avell.olatunde@gmail.com>
2023-09-28 00:08:39 +0000

    js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,5 @@
 function signUpUser(firstName, lastName) {
-  return new Promise((resolve) => {
-    resolve({ fisrtName: firstName, lastName });
-  });
+  return Promise.resolve({ fisrtName: firstName, lastName }); 
 }
 
 export default signUpUser;
