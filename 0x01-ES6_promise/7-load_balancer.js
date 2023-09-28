f475a0f18216a6f8a5c13154edfb8d5e48df6b47
Timothy <timothy.avell.olatunde@gmail.com>
2023-09-28 01:45:58 +0000

    promise.Race

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,3 +1,8 @@
+process.on('unhandledRejection', (err) => {
+  // Handle unhandled promise rejections here
+  console.error(err);
+});
+
 export default function loadBalancer(chinaDownload,USDownload){
 	return Promise.race([chinaDownload,USDownload])
 }
