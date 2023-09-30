f7857f2ed0a5033cb04633189fdd4ce98a980c33
Timothy <timothy.avell.olatunde@gmail.com>
2023-09-30 01:16:05 +0000

    classes

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,22 +1,23 @@
-import Building from './5-building'
-class SkyHighBuilding {
-  constructor(sqft, floors) {
-    super(sqft)
-    this._floors = floors;
-  }
+import Building from './5-building';
 
-  get sqft() {
-    return this._sqft;
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
-  excuationWarningMessage() {
-    return 'Evacuate slowly the NUMBER_OF_FLOORS floors'
+  set floors(newFloors) {
+    if (typeof newFloors !== 'number') {
+      throw TypeError('Floors must be a number');
+    }
+    this._floors = newFloors;
   }
 
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
-export default  SkyHighBuilding;
-
