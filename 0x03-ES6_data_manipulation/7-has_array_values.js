60452d76bd4b6b842945c5d009b07aa1c23b36f4
Timothy <timothy.avell.olatunde@gmail.com>
2023-10-03 02:14:35 +0000

    set

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,12 @@
+export default function  hasValueFromArray(set, array) {
+  if(array.length === 1){
+    for(const element of array){
+      if (!set.has(element)){
+        return false;
+    }
+  return true;
+  }
+  }else{
+    return false;
+  }
+}
