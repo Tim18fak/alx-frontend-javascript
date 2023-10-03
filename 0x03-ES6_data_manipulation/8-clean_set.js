cecca5df609e9245d5420b23d05b3fd9f26f0031
Timothy <timothy.avell.olatunde@gmail.com>
2023-10-03 11:58:41 +0000

    set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+  let string = [];
+  let returnValue;
+  if(!startString){
+   return '';
+  }
+  for(let val of set) {
+    if(val.startsWith(startString)){
+      string.push(val.slice(startString.length));
+      returnValue = string.join('-');
+    }
+  }
+  return returnValue
+}
